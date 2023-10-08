ae0000879bed24a3e88064c36765945377ab08ef
Amey <ameygaikar28@gmail.com>
2023-10-08 20:46:09 +0530

    Create base class and Bollinger Bands

---
+++ Entity.py
@@ -0,0 +1,71 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import mplfinance as mpl
+from pyti.bollinger_bands import upper_bollinger_band, lower_bollinger_band, middle_bollinger_band
+
+class Entity:
+    def __init__(self, df_daily: pd.DataFrame, df_intraday: pd.DataFrame):
+        self.df_daily = df_daily
+        self.df_intraday = df_intraday
+        self.create_bollinger_bands_intraday()
+        self.create_bollinger_bands_daily()
+        self.detect_points_intraday()
+        self.detect_points_daily()
+
+    def create_bollinger_bands_intraday(self, WINDOW=7):
+        self.df_intraday['mbb'] = middle_bollinger_band(self.df_intraday['close'], WINDOW)
+        self.df_intraday['ubb'] = upper_bollinger_band(self.df_intraday['close'], WINDOW)
+        self.df_intraday['lbb'] = lower_bollinger_band(self.df_intraday['close'], WINDOW)
+
+    def create_bollinger_bands_daily(self, WINDOW=20):
+        self.df_daily['mbb'] = middle_bollinger_band(self.df_daily['close'], WINDOW)
+        self.df_daily['ubb'] = upper_bollinger_band(self.df_daily['close'], WINDOW)
+        self.df_daily['lbb'] = lower_bollinger_band(self.df_daily['close'], WINDOW)
+
+    def plot_bollinger_bands_intraday(self):
+        plt.title('Intraday Bollinger Bands')
+        plt.plot(self.df_intraday['close'], c='blue')
+        plt.plot(self.df_intraday['mbb'], c='grey')
+        plt.plot(self.df_intraday['ubb'], c='green')
+        plt.plot(self.df_intraday['lbb'], c='red')
+
+    def plot_bollinger_bands_daily(self):
+        plt.title('20-Day Bollinger Bands')
+        plt.plot(self.df_daily['close'], c='blue')
+        plt.plot(self.df_daily['mbb'], c='grey')
+        plt.plot(self.df_daily['ubb'], c='green')
+        plt.plot(self.df_daily['lbb'], c='red')
+
+    def mpl_intraday_plot(self, type='candle', mav=()): 
+        mpl.plot(self.df_intraday.set_index(pd.to_datetime(self.df_intraday['timestamp'])), type=type, mav=mav)
+
+    def mpl_daily_plot(self, type='candle', mav=()): 
+        mpl.plot(self.df_daily.set_index(pd.to_datetime(self.df_daily['timestamp'])), type=type, mav=mav)
+
+    def detect_points_intraday(self):
+        self.BUY_POINTS_INTRADAY = np.array(self.df_intraday[(self.df_intraday['close'] < self.df_intraday['lbb'])]['timestamp'])
+        self.SELL_POINTS_INTRADAY = np.array(self.df_intraday[(self.df_intraday['close'] > self.df_intraday['ubb'])]['timestamp'])
+        # self.buy_df_intraday = self.df_intraday[(self.df_intraday['close'] < self.df_intraday['lbb'])][['open','high','low', 'close']]
+        # self.sell_df_intraday = self.df_intraday[(self.df_intraday['close'] > self.df_intraday['ubb'])][['open','high','low', 'close']]
+
+    def detect_points_daily(self):
+        self.BUY_POINTS_DAILY = np.array(self.df_daily[(self.df_daily['close'] < self.df_daily['lbb'])]['timestamp'])
+        self.SELL_POINTS_DAILY = np.array(self.df_daily[(self.df_daily['close'] > self.df_daily['ubb'])]['timestamp'])
+        # self.buy_df_daily = self.df_daily[(self.df_daily['close'] < self.df_daily['lbb'])][['open','high','low', 'close']]
+        # self.sell_df_daily = self.df_daily[(self.df_daily['close'] > self.df_daily['ubb'])][['open','high','low', 'close']]
+
+    def plot_points_intraday(self):
+        plt.plot(self.df_intraday['close'])
+        for n in range(len(self.BUY_POINTS_INTRADAY)):
+            plt.plot(self.df_intraday[self.df_intraday['timestamp'] == self.BUY_POINTS_INTRADAY[n]]['close'], '^', color='g')
+        for n in range(len(self.SELL_POINTS_INTRADAY)):
+            plt.plot(self.df_intraday[self.df_intraday['timestamp'] == self.SELL_POINTS_INTRADAY[n]]['close'], 'v', color='r')
+
+    def plot_points_daily(self):
+        plt.plot(self.df_daily['close'])
+        for n in range(len(self.BUY_POINTS_DAILY)):
+            plt.plot(self.df_daily[self.df_daily['timestamp'] == self.BUY_POINTS_DAILY[n]]['close'], '^', color='g')
+        for n in range(len(self.SELL_POINTS_DAILY)):
+            plt.plot(self.df_daily[self.df_daily['timestamp'] == self.SELL_POINTS_DAILY[n]]['close'], 'v', color='r')
+    
